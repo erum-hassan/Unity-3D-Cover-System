617728b79d5e17a71f154188687d48c2842e47eb
Alex Ling <hkalexling@gmail.com>
2015-05-22 01:49:27 +0800

    Initial Commit

---
+++ Script/AITakeCover.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AITakeCover : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject[] cover;
+
+	public float detectAngle = 120f;
+	public float detectDistance = 20f;
+	public float sideDetectDistance = 5f;
+	public bool notice;
+
+	public int coverNum = -1;
+	
+	public bool inCover;
+
+	public NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		nav = this.GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerVector = player.transform.position - transform.position;
+		float distance = Vector3.Distance (this.transform.position, player.transform.position);
+		if ((((Mathf.Abs ((int)Vector3.Angle (this.transform.forward, playerVector)) < detectAngle / 2f) && (distance < detectDistance))||(distance < sideDetectDistance))
+		    && Facing()) {
+			notice = true;
+		}
+		else{
+			notice = false;
+		}
+
+		if (notice) {
+			TakeCover ();
+			detectDistance = 1000f;
+			sideDetectDistance = 1000f;
+		}
+
+		if (coverNum >= 0) {
+			nav.SetDestination (cover [coverNum].transform.position);
+		}
+
+		if(inCover){
+			FacePlayer();
+		}
+	}
+
+	//Find the closest, empty and safe cover
+	void TakeCover(){
+		float[] coverDistance = new float[cover.Length];
+		for(int i=0; i<cover.Length; i++){
+			coverDistance[i] = Vector3.Distance(this.transform.position, cover[i].transform.position);
+		}
+		float[] sortedDistance = (float[])coverDistance.Clone ();
+		System.Array.Sort (sortedDistance);
+		for (int i=0; i<cover.Length; i++) {
+			int j = System.Array.IndexOf(coverDistance, sortedDistance[i]);
+			CoverScript coverscript = cover[j].GetComponent<CoverScript>();
+			if(coverscript.safe && !coverscript.full){
+				coverNum = j;
+				break;
+			}
+		}
+	}
+
+	bool Facing(){
+		RaycastHit hit;
+		Vector3 playerVector = player.transform.position - transform.position;
+		return (Physics.Raycast (transform.position, playerVector, out hit)) && (hit.transform.gameObject.tag == "Player");
+	}
+
+	void FacePlayer(){
+		Vector3 lookPosition = player.transform.position - transform.position;
+		lookPosition.y = 0;
+		Quaternion rotation = Quaternion.LookRotation (lookPosition);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 10f);
+	}
+}
---
+++ Script/CoverScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoverScript : MonoBehaviour {
+
+	public GameObject player;
+	public float maxLength = 100f;
+	public bool safe;
+	public bool full;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, (player.transform.position - transform.position), out hit, maxLength)) {
+			if (hit.transform.gameObject.tag == "Player"){
+				safe = false;
+			}
+			else{
+				safe = true;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		full = true;
+		if (other.gameObject.tag == "Enemy") {
+			AITakeCover aiTakeCover = other.gameObject.GetComponent<AITakeCover>();
+			aiTakeCover.inCover = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		full = false;
+		if (other.gameObject.tag == "Enemy") {
+			AITakeCover aiTakeCover = other.gameObject.GetComponent<AITakeCover>();
+			aiTakeCover.inCover = false;
+		}
+	}
+}
